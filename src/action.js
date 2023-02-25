8cad8e0bc233f619f176f4b37e5643b0767e3d6d
Gerald Ruiz <gruizcavero@uh.edu>
2023-02-25 14:12:11 -0600

    pr ready

--- src/action.js
+++ src/action.js
@@ -1,5 +1,27 @@
+const core = require('@actions/core');
+const github = require('@actions/github')
+
+
 async function run() {
-    console.log("Hello World");
+
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+
+    octokit.rest.issues.createComment({
+        ...context.repo,
+        issue_number,
+        body: 'yo your PR better be lit',
+    });
+
+    // we are getting the repo context because of destructuring on line 11
+
+
+
+
 }
 
 run();
